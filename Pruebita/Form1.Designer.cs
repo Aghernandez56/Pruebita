c61eb81894cdaac24925587bbc5952616280743d
Anette Gómez Hernández <112748@alumnouninter.mx>
2024-10-08 16:22:34 -0600

    label 1

--- Pruebita/Form1.Designer.cs
+++ Pruebita/Form1.Designer.cs
@@ -29,20 +29,35 @@ namespace Pruebita
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 171);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Form 1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
